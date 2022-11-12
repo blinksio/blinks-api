7a9809e513b181e18c76c4f72f58837687b0d386
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-11-12 16:41:44 +0000

    node: fetch related nodes

--- app/models/node.rb
+++ app/models/node.rb
@@ -16,4 +16,52 @@ class Node < ApplicationRecord
     node_data.save
     ActiveRecord::Base.logger = current_logger
   end
+
+  def transfer_addresses
+    # checking if result is cached
+    return Rails.cache.read("transfer_addresses:#{address}") if Rails.cache.exist?("transfer_addresses:#{address}")
+
+    return [] if node_data.blank? || node_data.transfers.blank?
+
+    Rails.cache.fetch("transfer_addresses:#{address}", expires_in: 1.day) do
+      node_data.transfers.map { |tx| tx['from'] } + node_data.transfers.map { |tx| tx['to'] }
+
+      # getting all wallet addresses from nft transfers
+      addresses = node_data.transfers.map { |tx| tx['from'] } + node_data.transfers.map { |tx| tx['to'] }
+
+      # ignoring minting addresses
+      addresses = addresses.reject { |address| address == '0x0000000000000000000000000000000000000000' }
+
+      # removing duplicates
+      addresses.uniq
+    end
+  end
+
+  def score_vs_node(other_node)
+    # calculating score based on number of common addresses
+    (transfer_addresses & other_node.transfer_addresses).count
+  end
+
+  def related_node_scores(refresh: false)
+    Rails.cache.fetch("node_scores:#{address}", expires_in: 1.day, force: refresh) do
+      # fetching all nodes with node data
+      nodes = Node.where(id: NodeData.pluck(:node_id)).where.not(id: id)
+
+      # calculating scores for all nodes
+      scores = nodes.map { |n| [n.id, score_vs_node(n)] }.to_h
+    end
+  end
+
+  def related_node_ids(refresh: false)
+    # filtering top 10 nodes with a score > 10
+    related_node_scores(refresh: refresh)
+      .select { |_, score| score > 10 }
+      .sort_by { |_, score| -score }
+      .map { |id, _| id }
+      .first(10)
+  end
+
+  def related_nodes
+    Node.where(id: related_node_ids)
+  end
 end
