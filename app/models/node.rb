8ca7b2e6ecf109734222dec8aef81805e533c983
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-11-12 20:07:38 +0000

    disable logging on update_nft_transfers

--- app/models/node.rb
+++ app/models/node.rb
@@ -9,7 +9,7 @@ class Node < ApplicationRecord
   def update_nft_transfers
     # disabling logging due to very verbosy output
     current_logger = ActiveRecord::Base.logger
-    # ActiveRecord::Base.logger = nil
+    ActiveRecord::Base.logger = nil
     transfers = EtherscanService.new.nft_transfers_of_contract(address)
     node_data = NodeData.find_or_create_by(node: self)
     node_data.transfers = transfers
