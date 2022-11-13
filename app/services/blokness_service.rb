faccf869341a9d77fe3b09ac7699df9c774c424e
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-11-13 13:32:00 +0000

    blokness: fetch historic nft transfers

--- app/services/blokness_service.rb
+++ app/services/blokness_service.rb
@@ -13,7 +13,32 @@ class BloknessService
     res['data']
   end
 
+  def nft_transfers_of_contract(contract_address)
+    uri = blokness_url + "/transactions?collection=#{contract_address}&limit=100"
+
+    res = request_blokness(uri)
+
+    data = format_nft_transfers(res['data'])
+
+    # iterating through cursor
+    while res['next']
+      uri = blokness_url + res['next']
+
+      res = request_blokness(uri)
+
+      data += format_nft_transfers(res['data'])
+    end
+
+    data
+  end
+
   private
+
+  def format_nft_transfers(transfers)
+    transfers.map do |transfer|
+      transfer.merge({ "from" => transfer['address_from'], "to" => transfer['address_to'] })
+    end
+  end
 
   def request_blokness(uri)
     response = HTTP
